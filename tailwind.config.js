37ea9157c95b2b6894e949f692600afeb08134fe
Keita <66842133+keitaabdou@users.noreply.github.com>
2021-08-05 17:45:35 +0000

    creation du projet

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'title': ['Sansita', 'sans-serif'],
+        'body': ['Lato', 'sans-serif']
+      },
+      color: {
+        'pri': '#081FAD',
+        'sec': '#0083FF',
+        'gen': '#3B4D71',
+        'our-bg': '#D9E6FF'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
