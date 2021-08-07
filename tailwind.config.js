c68a6105e5c2d5eb3290d2e4d2ae9ba36142ec9d
Keita <66842133+keitaabdou@users.noreply.github.com>
2021-08-07 22:40:54 +0000

    Ajout des différents pages et rende le site responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
     extend: {
       fontFamily: {
         'title': ['Sansita', 'sans-serif'],
-        'body': ['Lato', 'sans-serif']
+        'body' : ['Lato','sans-serif']
       },
-      color: {
-        'pri': '#081FAD',
+      colors: {
+        'pri': '#081F4D',
         'sec': '#0083FF',
         'gen': '#3B4D71',
         'our-bg': '#D9E6FF'
